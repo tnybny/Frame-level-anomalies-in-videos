6b50960acb95d174c543fa69ccba2fa7d82e71b5
tnybny <tnybny@gmail.com>
2018-02-21 17:45:26 -0500

    make temporal augmentation a configurable parameter and data iterator use temporal volume inputted

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
     LAMBDA = float(Config.get("Default", "LAMBDA"))
     BATCH_SIZE = int(Config.get("Default", "BATCH_SIZE"))
     TVOL = int(Config.get("Default", "TVOL"))
+    TAUG = bool(int(Config.get("Default", "AUG")))
     P_TRAIN = Config.get("Default", "P_TRAIN")
     P_TEST = Config.get("Default", "P_TEST")
     P_LABELS = Config.get("Default", "P_LABELS")
@@ -35,8 +36,7 @@ if __name__ == "__main__":
     model_path = os.path.join("models", METHOD, dt)
     os.makedirs(model_path)
 
-    d = DataIterator(P_TRAIN, P_TEST, P_LABELS, batch_size=BATCH_SIZE)
-    clip_params = np.load(P_CLIP_PARAMS)
+    d = DataIterator(P_TRAIN, P_TEST, P_LABELS, batch_size=BATCH_SIZE, tvol=TVOL, taug=TAUG)
     if METHOD == 'STAE':
         net = SpatialTemporalAutoencoder(tvol=TVOL, alpha=ALPHA, batch_size=BATCH_SIZE, lambd=LAMBDA)
     elif METHOD == 'CONVAE2D':
--- src/data_iterator.py
+++ src/data_iterator.py
@@ -1,14 +1,15 @@
 import numpy as np
 
 FRAMES_PER_VIDEO = 200
-TVOL = 10
 
 
 class DataIterator(object):
-    def __init__(self, p_train, p_test, p_labels, batch_size, stride=1):
+    def __init__(self, p_train, p_test, p_labels, batch_size, stride=1, tvol=10, taug=False):
         self.train, self.test, self.labels = np.load(p_train), np.load(p_test), np.load(p_labels)
         self._index = 0
-        if stride > TVOL:
+        self._tvol = tvol
+        self._taug = taug
+        if stride > tvol:
             raise ValueError('The stride can not be greater than temporal volume!')
         self._stride = stride
         self.batch_size = batch_size
@@ -18,12 +19,15 @@ class DataIterator(object):
         create volumes of videos with temporal augmentation at random
         :return: self.batch_size volumes of videos
         """
-        batch = np.zeros(shape=(self.batch_size, TVOL) + self.train[0].shape)
+        aug_idx = 1
+        batch = np.zeros(shape=(self.batch_size, self._tvol) + self.train[0].shape)
         for i in xrange(self.batch_size):
             vid_idx = np.random.randint(0, self.train.shape[0] / FRAMES_PER_VIDEO)
-            frame_idx = np.random.randint(0, FRAMES_PER_VIDEO - TVOL)
+            if self._taug:
+                aug_idx = np.random.randint(1, 4)
+            frame_idx = np.random.randint(0, FRAMES_PER_VIDEO - self._tvol)
             batch[i] = self.train[(FRAMES_PER_VIDEO * vid_idx + frame_idx):
-                                  (FRAMES_PER_VIDEO * vid_idx + frame_idx + TVOL)]
+                                  (FRAMES_PER_VIDEO * vid_idx + frame_idx + self._tvol * aug_idx):aug_idx]
         return batch
 
     def get_test_batch(self):
@@ -32,14 +36,14 @@ class DataIterator(object):
         until test set is exhausted
         :return: self.batch_size volumes of videos and index for every frame in these volumes
         """
-        batch = np.zeros(shape=(self.batch_size, TVOL) + self.test[0].shape)
-        frame_indices = np.full(shape=(self.batch_size, TVOL), fill_value=-1, dtype=np.int)
+        batch = np.zeros(shape=(self.batch_size, self._tvol) + self.test[0].shape)
+        frame_indices = np.full(shape=(self.batch_size, self._tvol), fill_value=-1, dtype=np.int)
         for i in xrange(self.batch_size):
             if not self.check_data_exhausted():
-                if self._index % FRAMES_PER_VIDEO + TVOL > FRAMES_PER_VIDEO:
+                if self._index % FRAMES_PER_VIDEO + self._tvol > FRAMES_PER_VIDEO:
                     self._index = (self._index / FRAMES_PER_VIDEO + 1) * FRAMES_PER_VIDEO
-                batch[i] = self.test[self._index:(self._index + TVOL)]
-                frame_indices[i] = np.arange(self._index, self._index + TVOL)
+                batch[i] = self.test[self._index:(self._index + self._tvol)]
+                frame_indices[i] = np.arange(self._index, self._index + self._tvol)
                 self._index += self._stride
             else:
                 break
@@ -55,7 +59,7 @@ class DataIterator(object):
         return self.test.shape[0]
 
     def check_data_exhausted(self):
-        return self._index + TVOL > self.test.shape[0]
+        return self._index + self._tvol > self.test.shape[0]
 
     def reset_index(self):
         self._index = 0
