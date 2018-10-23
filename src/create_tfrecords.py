b5701d7db7e867616ed2eb8f50679a005e2ada6b
tnybny <tnybny@gmail.com>
2018-10-23 13:13:54 -0400

    show eval directory, but hide confidential trivial frame and pixel auc code

---
+++ src/create_tfrecords.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+from PIL import Image
+import os
+from glob import glob
+import numpy as np
+import re
+import ConfigParser
+import tensorflow as tf
+
+
+Config = ConfigParser.ConfigParser()
+config_path = os.path.join("config", "config.ini")
+Config.read(config_path)
+data_dir = Config.get("Default", "DATA_DIR")
+ext = Config.get("Default", "EXT")
+
+train_dir = os.path.join(data_dir, "Train")
+test_dir = os.path.join(data_dir, "Test")
+TVOL = int(Config.get("Default", "TVOL"))
+
+train_dirs = sorted([os.path.join(train_dir, d) for d in os.listdir(train_dir) if re.match(r'Train[0-9][0-9][0-9]$',
+                                                                                           d)])
+assert len(train_dirs) >= 1
+test_dirs = sorted([os.path.join(test_dir, d) for d in os.listdir(test_dir) if re.match(r'Test[0-9][0-9][0-9]$', d)])
+assert len(test_dirs) >= 1
+train_filename = os.path.join('data.nosync', 'train.tfrecords')
+test_filename = os.path.join('data.nosync', 'test.tfrecords')
+
+for split in ['Train', 'Test']:
+    dirs = train_dirs if split is 'Train' else test_dirs
+    writer = tf.python_io.TFRecordWriter(train_filename) if split is 'Train' \
+        else tf.python_io.TFRecordWriter(test_filename)
+    for seq_idx in range(len(dirs)):
+        print("Reading images from directory:", dirs[seq_idx])
+        fnames = sorted(glob(os.path.join(dirs[seq_idx], '*.' + ext)))
+        i = 0
+        im = np.array(Image.open(fnames[0]))
+        while i <= len(fnames) - TVOL:
+            print("frame", i)
+            ims = np.stack([np.array(Image.open(x)) for x in fnames[i:i + TVOL]], axis=0).astype('uint8')
+            if im.ndim == 2:  # add dimension to grayscale image
+                ims = np.expand_dims(ims, axis=3)
+            i += 1
+            feature = {'vid_clip': tf.train.Feature(bytes_list=
+                                                    tf.train.BytesList(value=[tf.compat.as_bytes(ims.tostring())])),
+                       'nchannels': tf.train.Feature(int64_list=tf.train.Int64List(value=[ims.shape[3]])),
+                       'height': tf.train.Feature(int64_list=tf.train.Int64List(value=[im.shape[0]])),
+                       'width': tf.train.Feature(int64_list=tf.train.Int64List(value=[im.shape[1]]))}
+
+            # Create an example protocol buffer
+            example = tf.train.Example(features=tf.train.Features(feature=feature))
+
+            # Serialize to string and write on the file
+            writer.write(example.SerializeToString())
+    writer.close()
